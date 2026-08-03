78627edbe4b6b570f16f4f123dbd047d7c49a25b
uzairshah9971-dev <uzairshah9971@gmail.com>
2026-08-03 23:32:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const button=document.getElementById("startBtn");
 
+button.onclick=function(){
+
+let name=document.getElementById("playerName").value.trim();
+
+if(name==""){
+
+alert("Please enter your name.");
+
+return;
+
+}
+
+localStorage.setItem("playerName",name);
+
+window.location.href="pages/home.html";
+
+}
