d6581dd81fec7b6d3a778c6b1445d68d9f2db90e
uzairshah9971-dev <uzairshah9971@gmail.com>
2026-08-03 23:48:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,70 @@
+function startTraining(){
 
-const button=document.getElementById("startBtn");
+    let name = document.getElementById("playerName").value;
 
-button.onclick=function(){
 
-let name=document.getElementById("playerName").value.trim();
+    if(name.trim() === ""){
+        alert("Please enter your name ⚽");
+        return;
+    }
 
-if(name==""){
 
-alert("Please enter your name.");
+    document.querySelector(".profile").style.display="none";
 
-return;
 
-}
+    let dashboard = document.getElementById("dashboard");
 
-localStorage.setItem("playerName",name);
+    dashboard.classList.remove("hidden");
 
-window.location.href="pages/home.html";
 
+    dashboard.innerHTML = `
+
+    <h2>Welcome ${name} 👋</h2>
+
+    <p>
+    Start your football training journey!
+    </p>
+
+
+    <div class="cards">
+
+        <div class="card">
+        🎯
+        <h3>Shooting</h3>
+        </div>
+
+
+        <div class="card">
+        👟
+        <h3>Dribbling</h3>
+        </div>
+
+
+        <div class="card">
+        ⚽
+        <h3>Passing</h3>
+        </div>
+
+
+        <div class="card">
+        🏃
+        <h3>Speed</h3>
+        </div>
+
+
+        <div class="card">
+        💪
+        <h3>Fitness</h3>
+        </div>
+
+
+        <div class="card">
+        🍎
+        <h3>Nutrition</h3>
+        </div>
+
+
+    </div>
+
+    `;
 }
