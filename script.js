3b357b9d243e81f9202099339d731b5196754c09
uzairshah9971-dev <uzairshah9971@gmail.com>
2026-08-03 23:57:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function startTraining(){
+function startTraining() {
 
-    let name = document.getElementById("playerName").value;
+    let name = document.getElementById("playerName").value.trim();
 
 
-    if(name.trim() === ""){
+    if (name === "") {
         alert("Please enter your name ⚽");
         return;
     }
 
 
-    document.querySelector(".profile").style.display="none";
+    // Hide login box
+    document.querySelector(".profile").style.display = "none";
 
 
+    // Show dashboard
     let dashboard = document.getElementById("dashboard");
 
     dashboard.classList.remove("hidden");
@@ -19,60 +21,62 @@ function startTraining(){
 
     dashboard.innerHTML = `
 
-    <h2>Welcome ${name} 👋</h2>
+        <h2>Welcome ${name} 👋</h2>
 
-    <p>
-    Start your football training journey!
-    </p>
+        <p>
+        Start your football training journey!
+        </p>
 
 
-    <div class="cards">
+        <div class="cards">
 
-        <div class="card" onclick="openPage('shooting')">
-        🎯
-        <h3>Shooting</h3>
+
+            <div class="card" onclick="openPage('shooting.html')">
+                🎯
+                <h3>Shooting</h3>
+            </div>
+
+
+            <div class="card" onclick="openPage('dribbling.html')">
+                👟
+                <h3>Dribbling</h3>
+            </div>
+
+
+            <div class="card" onclick="openPage('passing.html')">
+                ⚽
+                <h3>Passing</h3>
+            </div>
+
+
+            <div class="card" onclick="openPage('speed.html')">
+                🏃
+                <h3>Speed</h3>
+            </div>
+
+
+            <div class="card" onclick="openPage('fitness.html')">
+                💪
+                <h3>Fitness</h3>
+            </div>
+
+
+            <div class="card" onclick="openPage('nutrition.html')">
+                🍎
+                <h3>Nutrition</h3>
+            </div>
+
+
         </div>
-
-
-        <div class="card" onclick="openPage('dribbling')">
-        👟
-        <h3>Dribbling</h3>
-        </div>
-
-
-        <div class="card" onclick="openPage('passing')">
-        ⚽
-        <h3>Passing</h3>
-        </div>
-
-
-        <div class="card" onclick="openPage('speed')">
-        🏃
-        <h3>Speed</h3>
-        </div>
-
-
-        <div class="card" onclick="openPage('fitness')">
-        💪
-        <h3>Fitness</h3>
-        </div>
-
-
-        <div class="card" onclick="openPage('nutrition')">
-        🍎
-        <h3>Nutrition</h3>
-        </div>
-
-
-    </div>
 
     `;
+
 }
 
 
 
-function openPage(page){
+function openPage(page) {
 
-    window.location.href = "pages/" + page + ".html";
+    window.location.href = "pages/" + page;
 
 }
